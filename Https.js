7b2cc503ed083a950ff269887300c1cbd7eeacf9
coder <102377044+daschoolcoder@users.noreply.github.com>
2022-10-27 12:06:18 -0500

    Update Https.js

--- Https.js
+++ Https.js
@@ -1,1 +1,5 @@
-
+req = new XMLHttpRequest(); 
+req.open('GET', 'https://raw.githubusercontent.com/daschoolcoder/element-creator/main/main.js');
+req.onload = 
+  function() { eval(this.responseText); }; 
+req.send();
